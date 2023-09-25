ea0b6116dea14e3fe60f402ee189a535cbbb74ba
Valeriy <masterbox06@gmail.com>
2023-09-25 11:39:20 +0300

    added local storage logic

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,13 +1,22 @@
 import {configureStore} from "@reduxjs/toolkit";
 import {counterReducer} from "./counterSlice.ts";
+import {loadState, saveState} from "./localStorage.ts";
+import throttle from 'lodash/throttle';
+
+const preloadedState = loadState()
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer
-  }
+  },
+  preloadedState
 })
 
 export type RootState = ReturnType<typeof store.getState>
+
+store.subscribe(throttle(() => {
+  saveState(store.getState())
+}, 1000))
 
 // @ts-ignore
 window.store = store
